eef75cd813c6376694ea644abc88756de177de78
Mario galdon <mariodesmo13@gmail.com>
2022-10-05 09:50:30 +0100

    creado parte de artista album ,etc

---
+++ Controllers/AlbumController.cs
@@ -0,0 +1,47 @@
+﻿using AT02_4_GaldonMario_Musica.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace AT02_4_GaldonMario_Musica.Controllers
+{
+    public class AlbumController : Controller
+    {
+        private readonly ChinookContext _context;
+
+        public AlbumController(ChinookContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Album> listaCanciones = _context.Albums.Include(Album => Album.Artist).OrderByDescending(Album => Album.Title).Take(15).ToList();
+            return View(listaCanciones);
+        }
+
+        [HttpGet]
+        public IActionResult CreateAlbum()
+        {
+            ViewBag.ArtistId = new SelectList(_context.Artists, "ArtistId", "Name");
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateAlbum(Album album)
+        {
+            album.AlbumId = _context.Albums.Max(Album => Album.AlbumId) + 1;
+            if (ModelState.IsValid)
+            {
+                _context.Albums.Add(album); 
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+            
+        }
+    }
+}
---
+++ Controllers/ArtistsController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AT02_4_GaldonMario_Musica.Models;
+
+namespace AT02_4_GaldonMario_Musica.Controllers
+{
+    public class ArtistsController : Controller
+    {
+        private readonly ChinookContext _context;
+
+        public ArtistsController(ChinookContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Artists
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.Artists.ToListAsync());
+        }
+
+        // GET: Artists/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Artists == null)
+            {
+                return NotFound();
+            }
+
+            var artist = await _context.Artists
+                .FirstOrDefaultAsync(m => m.ArtistId == id);
+            if (artist == null)
+            {
+                return NotFound();
+            }
+
+            return View(artist);
+        }
+
+        // GET: Artists/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Artists/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Name")] Artist artist)
+        {
+            artist.ArtistId= _context.Artists.Max(Artist => Artist.ArtistId)+1;
+            if (ModelState.IsValid)
+            {
+                _context.Add(artist);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(artist);
+        }
+
+        // GET: Artists/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Artists == null)
+            {
+                return NotFound();
+            }
+
+            var artist = await _context.Artists.FindAsync(id);
+            if (artist == null)
+            {
+                return NotFound();
+            }
+            return View(artist);
+        }
+
+        // POST: Artists/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ArtistId,Name")] Artist artist)
+        {
+            if (id != artist.ArtistId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(artist);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ArtistExists(artist.ArtistId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(artist);
+        }
+
+        // GET: Artists/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Artists == null)
+            {
+                return NotFound();
+            }
+
+            var artist = await _context.Artists
+                .FirstOrDefaultAsync(m => m.ArtistId == id);
+            if (artist == null)
+            {
+                return NotFound();
+            }
+
+            return View(artist);
+        }
+
+        // POST: Artists/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Artists == null)
+            {
+                return Problem("Entity set 'ChinookContext.Artists'  is null.");
+            }
+            var artist = await _context.Artists.FindAsync(id);
+            if (artist != null)
+            {
+                _context.Artists.Remove(artist);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ArtistExists(int id)
+        {
+          return _context.Artists.Any(e => e.ArtistId == id);
+        }
+    }
+}
